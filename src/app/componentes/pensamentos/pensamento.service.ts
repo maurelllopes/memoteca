fe8b59ba65e64a3fceb6046a6187ee55370aabaf
maurelllopes <maurelllopes@github.com>
2022-10-07 22:32:11 -0300

    Ajustes_8

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -4,32 +4,31 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
 
-  private readonly API = 'http://localhost:3000/pensamentos'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API)
-}
-criar(pensamento: Pensamento): Observable<Pensamento> {
-  return this.http.post<Pensamento>(this.API, pensamento)
-}
+    return this.http.get<Pensamento[]>(this.API);
+  }
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento);
+  }
 
-excluir(id: number): Observable<Pensamento> {
-  const url = `${this.API}/${id}`
-  return this.http.delete<Pensamento>(url)
-}
+  excluir(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
 
-buscarPorId(id: number): Observable<Pensamento> {
-  const url = `${this.API}/${id}`
-  return this.http.get<Pensamento>(url)
-}
-editar(pensamento: Pensamento): Observable<Pensamento> {
-  const url = `${this.API}/${pensamento.id}`
-  return this.http.put<Pensamento>(url, pensamento )
-}
+  buscarPorId(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
+  }
 }
